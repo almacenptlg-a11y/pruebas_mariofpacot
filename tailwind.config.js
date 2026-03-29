19f54dc7471b9e268cd31adc5fb343f2b4690d9a
almacenptlg-a11y <almacenptlg@gmail.com>
2026-03-28 21:43:32 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"], // Escanea tu HTML y JS actual
-  darkMode: 'class',
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
