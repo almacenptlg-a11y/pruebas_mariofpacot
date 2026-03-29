de1584598b720641095e5d94e90b2404d6b29b53
almacenptlg-a11y <almacenptlg@gmail.com>
2026-03-28 21:15:00 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // Escanea tu HTML y JS actual
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
